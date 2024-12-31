f1e56881ff242650b42292398a12b0da36504fcb
Jill Angel <tpennywise3@hotmail.com>
2024-12-31 00:52:33 -0500

    updgrade repo

---
+++ src/App.vue
@@ -0,0 +1,197 @@
+<template>
+<div class="container">
+  <div class="text-top">
+    <h1 class="unic-tittle">Venta de juegos de Steam <br>
+      <br>  
+      <img class="steam-logo" src="./assets/steam-icon.png" alt="">
+    </h1>
+  </div>
+  <div class="redes-style">
+    <a target="_blank" href="https://m.me/aleramrami/"><img class="redes" src="./assets/mensaje.png" alt=""></a>     
+    <a target="_blank" href="https://wa.me/573014983594"><img class="redes" src="./assets/wps.png" alt=""></a>   
+    <a target="_blank" href="https://www.facebook.com/groups/elmundodejill"><img class="redes" src="./assets/facebook.png" alt=""> </a>   
+  </div>
+  <div class="info">
+    <p class="text-info"> Si necesitas juegos, ítems o saldo steam a un bajo precio, aquí estoy yo para colaborarte, 
+      solo necesitas enviarme un mensaje, contáctate conmigo en alguna 
+      de las redes sociales que están en la parte superior, para que podamos negociar como va ser el proceso, anímate...</p>
+  </div>
+  <div class="gallery">
+    <gallgames/>
+  </div>
+  <!-- 
+  <div class="carrusel2">
+    hola culito
+  </div>
+  <div class="carrusel3">
+    hola culito
+  </div>
+  -->
+</div> 
+<footer class="down"><p class="parr-footer"> ⊂ (◉‿◉) つ Cllykoji Gameshop ❦ 2024 </p></footer>
+</template>
+
+<script>
+import gallgames from "./components/gall-games.vue"
+export default {
+  components: {
+    gallgames,
+  }
+}
+</script>
+
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  background-color: rgb(23, 33, 69);
+}
+.container{
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);;
+  grid-template-rows: repeat(5, 1fr);;
+  justify-items: center;
+  align-items: center; 
+  width: 100vh;
+  height: 100vh;
+}
+.text-top{
+  grid-column: 2/4;
+  grid-row: 2;
+  width:55vh;
+  height:auto;
+}
+.unic-tittle{
+  text-align: center;
+  color: rgb(255, 254, 254);
+  animation: fuego-azul 1s infinite alternate;
+}
+@keyframes fuego-azul {
+  0% {
+    color: #4287f5; /* Color inicial azul */
+    text-shadow: 0 0 10px #4287f5, 0 0 20px #4287f5, 0 0 40px #4287f5, 0 0 80px #4287f5;
+  }
+  100% {
+    color: #00ffff; /* Color final cyan */
+    text-shadow: 0 0 5px #4287f5, 0 0 10px #4287f5, 0 0 20px #2c6ac6, 0 0 40px #0088ff; 
+  }
+}
+.steam-logo{
+  width: auto;
+  height: 6em;
+}
+.steam-logo:hover{
+  animation: animacionImagen 3s infinite alternate;
+}
+@keyframes animacionImagen {
+  0% {
+    transform: scale(1); 
+  }
+  100% {
+    transform: scale(1.2); 
+  }
+}
+.redes-style {
+  display: flex;
+  justify-content: end;
+  grid-column: 6;
+  grid-row: 1;
+  width: 30vh;
+  height: 10vh;
+  padding-top: 5px;
+}
+.info{
+  grid-column: 1/5;
+  grid-row: 3;
+  color:rgb(255, 255, 255);
+  width: 100vh;
+  height: auto;
+  text-align: justify;
+  padding-bottom: 20px;
+}
+.text-info{
+  animation: text-change 3s infinite alternate; /* Animación de cambio de color durante 3 segundos, alternando infinitamente */
+}
+@keyframes text-change{
+  0% {
+    color: #c1b5b5; /* Blanco */
+  }
+  50% {
+    color: #c6d6f4; 
+  }
+  100% {
+    color: #ffffff;
+  }
+}
+.redes {
+  width: auto;
+  height: 2em;
+  padding: 5px;
+}
+.redes:hover{
+  width: auto;
+  height: 2.2em;
+}
+.down {
+  background-color:  rgb(15, 26, 66); /* Color de fondo con opacidad */
+  color: #ffffff;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  animation: slideUp 1.5s ease-in-out;
+}
+
+@keyframes slideUp {
+  0% {
+    transform: translateY(100%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+.parr-footer{
+  font-size: 13px;
+  text-align: center;
+  margin: 0;
+  padding: 5px;
+}
+.parr-footer:hover{
+  color: #4287f5;
+}
+.gallery{
+  grid-column: 1/4;
+  grid-row: 5;
+  width: 73vh;
+  height: 80vh;
+}
+/* .carrusel1{
+  grid-column: 1;
+  grid-row: 4;
+  background-color: rgb(65, 206, 81);
+  width: 25vh;
+  height: 25vh;
+}
+.carrusel2{
+  grid-column: 2;
+  grid-row: 4;
+  background-color: rgb(248, 0, 223);
+  width:  25vh;
+  height: 25vh;
+}
+.carrusel3{
+  grid-column: 3;
+  grid-row: 4;
+  background-color: rgb(255, 153, 0);
+  width: 25vh;
+  height: 25vh;
+}
+.carrusel4{
+  grid-column: 4;
+  grid-row: 4;
+  background-color: rgb(255, 0, 0);
+  width: 25vh;
+  height: 25vh;
+} */
+</style>
---
+++ src/components/gall-games.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="seccionG3">
+    <div class="grid-container">
+      <img class="grid-item hoverAll" src="../assets/fotos/lo logramos.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/descarga (1).jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/descarga.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Fly ✨.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/G L O R I O S O ✨.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/haikyuu.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Kageyama, Number Nine by aislingkiera.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Karasuno sticker (1).jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Karasuno Volleyball Logo Sticker by itsboattime.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Team Karasuno Magnet.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Karasuno.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/simbolitos chinos.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/asa ッ (closed).jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Atsumu.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/descarga (4).jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/descarga (3).jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/descarga (2).jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Haikyuuttt.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/DESENHANDO HINATA SHOYO DE HAIKYUU.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Haikyuu ONE-SHOTS [edición 2020] - Nishinoya Yū.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/letras rusas.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Wallpapers Haikyuu.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Реакция haikyuu _ волейбол.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Pegatinas_ Haikyuu.jpg">
+      <img class="grid-item hoverAll" src="../assets/fotos/_3.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/_33.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/4b1d0086-dacf-44ef-a9d6-9013d0b0c983.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/82f2a123-26f5-4721-9912-fc07ce53ba7f.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/37848cc9-1a28-44a1-bba0-d85aafcb165e.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/28876200-8f05-4688-8f1a-12c02d5226e2.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/alosed) (1).jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/asa closed).jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/asaclosed) (1).jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/asaclosed).jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/cara.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/dc762ac4-74c2-448f-becb-d0cf7ba59c27.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/h a i k y u u.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Haikyuu Wallpapers 30.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/hayyba.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/kageyama tobio  hiromizu.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Kageyama tobio.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Las mejores versiones de HaikyuuHappy Haikyuu.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/My pretty boy Akaashi keiji.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Nishnoya.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/noya.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Oiwaka Tooru.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/rojo123.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Segundos (Oikawa x Reader).jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/ShoyoHinata.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Sweatshirt Tendō.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Tanaka.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Your Haikyuu Life.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Your Haikyuu Lover.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/Yuu Nishinoya Icon.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/espaldaa.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/circulo22.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/123cancer.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/icons hinata.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/kurooo.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/123rojoo.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/pandalinn.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/asahi 123.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/keishi ukani.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/interest 3.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/kenma1.jpeg">
+      <img class="grid-item hoverAll" src="../assets/fotos/interes 245.jpeg">
+      </div>
+    </div>
+  </template>
+<script>
+export default {
+  data() 
+  {
+    return {
+    seccionG3Show: true,
+    seccionVideosShow: false,      
+  };
+  },
+  methods: {
+    clickChangeToVideo(componentName) {
+      this.seccionG3Show = false;
+      this.seccionVideosShow = false;  
+      switch (componentName) {
+        case 'grid-container':
+          this.seccionG3Show = true;
+          break;
+        case 'gridVideos':
+          this.seccionVideosShow = true;
+          break;
+      }
+    }
+  },
+}
+</script>
+<style>
+  .seccionG3 {
+    height: 66%;
+    width: 95vh;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+    color: white;
+    padding: 1em 0;
+    padding-left: 1em;
+    margin: 1em 0;
+    overflow-y: auto;
+  }
+  /* Estilos específicos para la barra de desplazamiento en navegadores WebKit en navegadores basados en WebKit (como Google Chrome y Safari)*/
+  .seccionG3::-webkit-scrollbar {
+    width: 0.5em; /* Ancho de la barra de desplazamiento */
+  }
+  .seccionG3::-webkit-scrollbar-thumb {
+    background-color: transparent; /* Color del pulgar de la barra de desplazamiento */
+}
+.seccionG3::-webkit-scrollbar-track {
+  background-color: transparent; /* Color de la pista de la barra de desplazamiento */
+}
+/* Estilos específicos para la barra de desplazamiento en navegadores Firefox */
+.seccionG3 {
+  scrollbar-width: thin; /* Ancho de la barra de desplazamiento */
+  scrollbar-color: transparent transparent; /* Color del pulgar y la pista de la barra de desplazamiento */
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 220px);
+  gap: 20px;
+  justify-content: center;
+  margin: 0 auto;
+}
+.grid-item {
+  cursor: pointer;
+  transition: filter 0.3s ease-in-out;
+  width: 38vh;
+  height: 25vh;
+  object-fit: cover;
+  background-color: lightgray;
+}
+.gridVideos{
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 300px);
+  gap: 20px;
+  height: 40em;
+  justify-content: center;
+  align-items: center;
+  overflow-y: auto;
+}
+.grid-itemVds {
+  cursor: pointer;
+  transition: filter 0.3s ease-in-out;
+  width: 300px;
+  height: 200px;
+  object-fit: cover;
+  background-color: lightgray;
+}
+Agrega estos estilos para el efecto de elevación y descripción
+.hoverAll {
+  position: relative;
+}
+.hoverAll:hover {
+  filter: brightness(80%); /* Puedes ajustar este valor según tus preferencias */
+  transform: translateY(-5px);
+}
+
+</style>
